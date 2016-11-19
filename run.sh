08c7850963f9be4113f874252ffee415a376335a
A. Datta <intersoftbengal@ymail.com>
2016-11-19 16:23:29 +0530

    Testing GitHub-Docker web hook

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ VOLUME_HOME="/var/lib/mysql"
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.0/apache2/php.ini # Path updated by A. Datta
-# Now we are going to bind to all interfaces for easy access. Do not do this on production srv unless MySQL is secured!
+# Now we are going to bind to all interfaces for easy access. Do not do this on production a server unless MySQL is secured!
 sed -ri -e "s/^bind-address.*/bind-address\t\t= 	0\.0\.0\.0/" /etc/mysql/mysql.conf.d/mysqld.cnf # Line added by A. Datta
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
